c3609386c9017563a39e36c46ab13219ea24e2a4
Tony Huang <t.tonyhuang@gmail.com>
2021-04-06 22:35:44 -0400

    mess with something

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,13 +3,9 @@ const axios = require("axios");
 require("dotenv").config();
 
 router.get("/get", (req, res) => {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=dc076026d26729debc1fda42cb98fcbc";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=${process.env.OPEN_WEATHER_KEY}`;
   axios
-    .get(
-      // `api.openweathermap.org/data/2.5/weather?q=6091104&appid=${process.env.OPEN_WEATHER_KEY}}`
-      url
-    )
+    .get(url)
     .then((response) => {
       res.status(200);
       res.send(JSON.stringify(response.data));
