1a296231cb3b51897c0948840d5b49c909423d92
Tony Huang <t.tonyhuang@gmail.com>
2021-05-14 00:41:12 -0400

    add icon render

--- client/src/apps/BibleStudy/components/Icon.tsx
+++ client/src/apps/BibleStudy/components/Icon.tsx
@@ -1,15 +1,26 @@
-import clearDay from "../../../images/01day.png";
-import clearNight from "../../../images/01night.png";
-import fewCloudsDay from "../../../images/02day.png";
-import fewCloudsNight from "../../../images/02night.png";
-import scatteredClouds from "../../../images/03day.png";
-import rain from "../../../images/09day.png";
-import snowDay from "../../../images/13day.png";
+import header1 from "../../../images/header1.png";
+import header2 from "../../../images/header2.png";
+import header3 from "../../../images/header3.png";
+import header4 from "../../../images/header4.png";
+import header5 from "../../../images/header5.png";
+import header6 from "../../../images/header6.png";
+import header7 from "../../../images/header7.png";
 
 export const renderHeaderIcon = (iconNumber:number) => {
     switch(iconNumber) {
         case 1:
-            return clearDay;
-        
+            return header1;
+        case 2:
+            return header2;
+        case 3:
+            return header3;
+        case 4:
+            return header4;
+        case 5: 
+            return header5;
+        case 6:
+            return header6;
+        case 7:
+            return header7;
     }
 }
