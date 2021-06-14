a0666d12a12e33c03b3ec9873d0a58a1273668ab
Tony Huang <t.tonyhuang@gmail.com>
2021-06-14 14:02:57 -0400

    add highlight value

--- client/src/apps/HighlightPage/components/HighlightDisplay.tsx
+++ client/src/apps/HighlightPage/components/HighlightDisplay.tsx
@@ -1,16 +1,28 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import '../styles/HighlightDisplay.css'
 
 const HighlightDisplay:FC = () => {
+    const [highlight, setHighlight] = useState<string|null>(null);
+
+    const handleChange = (e:any) => {
+        setHighlight(e.target.value);
+    }
+
+    const handleClick = () => {
+        console.log(highlight);
+    }
+
+
+
     return (
         <div className = "flowPage">
             <div className="card">
                 <h2>Highlight</h2>
                 <label className="input">
-                <input className="inputField" type="text" placeholder=" " />
+                <input className="inputField" type="text" placeholder="Whats your highlight" onChange = {(e)=>handleChange(e)} />
                 </label>
                 <div className="button-group">
-                <button className = 'send'>Send</button>
+                <button className = 'send' onClick = {() => handleClick()}>Send</button>
                 </div>
             </div>
         </div>
